353a17d4bb409cd3651647dcf18cd1278c3c0589
vickyjm <mvjorgemauriello@gmail.com>
2015-08-18 22:59:51 -0430

    Iniciar sesión listo. Falta poner htmls

--- Mantenimiento/Mantenimiento/urls.py
+++ Mantenimiento/Mantenimiento/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from app_HOI.views import *
 
 urlpatterns = patterns('',
     # Examples:
@@ -7,4 +8,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$',inicio_sesion,name = "inicio_sesion"),
 )
--- Mantenimiento/app_HOI/views.py
+++ Mantenimiento/app_HOI/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from app_HOI.forms import * 
+from app_HOI.models import *
 
-# Create your views here.
+# Vista usada al iniciar el sistema
+def inicio_sesion(request):
+    if request.method == 'POST':
+        form = iniciarSesionForm(request.POST)
+        if form.is_valid():
+            # Verifico si el usuario existe, esté activo o no
+            user = authenticate(username = form.cleaned_data['cedula'],password = form.cleaned_data['contraseña'])
+            if user is not None:
+                if user.is_active:
+                    login(request, user)
+                    print("Te loggeaste") # Aqui iria al siguiente html?
+                else:
+                    print("No estas activo") # Aqui envia un mensaje en el html de que no esta activo
+            else:
+                print("Usuario o contraseña mala") # Aqui envia un mensaje en el html de que puso las cosas mal
+    else:
+        form = iniciarSesionForm()
+    return render(request, 'prueba.html', {'form': form}) # Cambiar el nombre del html
