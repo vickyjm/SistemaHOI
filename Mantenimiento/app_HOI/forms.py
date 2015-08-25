f9658a4b8a31e53eecfda33c0f149a210944ea10
“Patricia“ <patricia.reinoso12@gmail.com>
2015-08-25 17:15:06 -0430

    item form y template de crear item

--- Mantenimiento/app_HOI/forms.py
+++ Mantenimiento/app_HOI/forms.py
@@ -2,6 +2,8 @@
 
 from django import forms
 from django.core.validators import RegexValidator
+from app_HOI.models import Categoria
+from django.forms import ModelChoiceField
 
 class iniciarSesionForm(forms.Form):
     cedula = forms.CharField(
@@ -24,6 +26,7 @@ class registroForm(forms.Form):
                     max_length = 11,
                     required = True,
                     label = "Cédula de Identidad",
+                    widget=forms.TextInput(attrs={'style': 'width:100%'}),
                     validators = [
                         RegexValidator(
                             regex = '^([1-9][0-9]{0,2})([0-9]{3}){0,3}$',
@@ -31,23 +34,24 @@ class registroForm(forms.Form):
                         )
                     ]
             )
-    nombre = forms.CharField(max_length = 30, required = True, label = "Nombre")
-    apellido = forms.CharField(max_length = 30, required = True, label = "Apellido")
-    correo = forms.EmailField(required = False, label = "Correo electrónico")
+    nombre = forms.CharField(max_length = 30, required = True, label = "Nombre", widget=forms.TextInput(attrs={'style': 'width:100%'}))
+    apellido = forms.CharField(max_length = 30, required = True, label = "Apellido", widget=forms.TextInput(attrs={'style': 'width:100%'}))
+    correo = forms.EmailField(required = False, label = "Correo electrónico", widget=forms.EmailInput(attrs={'style': 'width:100%'}))
     tipoEmpleado = (("tecnico","Técnico"), ("almacenista", "Almacenista"), ("admin", "Administrador"))
     tipo = forms.ChoiceField(required = True, choices = tipoEmpleado, 
-                             widget = forms.Select(), label = "Cargo")
+                             widget = forms.Select(attrs={'style': 'width:100%; background-color:white'}), label = "Cargo")
     contraseña1 = forms.CharField(max_length = 25,
                                  required = True,
-                                 label = "Contraseña", widget=forms.PasswordInput)
+                                 label = "Contraseña", widget=forms.PasswordInput(attrs={'style': 'width:100%'}))
     contraseña2 = forms.CharField(max_length = 25,
                                  required = True,
-                                 label = "Confirmar contraseña", widget=forms.PasswordInput)
+                                 label = "Confirmar contraseña", widget=forms.PasswordInput(attrs={'style': 'width:100%'}))
 
-class itemForm(forms.Form)
-    nombre = forms.CharField(max_length = 60, required = True, label = "Nombre")
-    cantidad = forms.IntegerField(max_value = 2147483647, min_value = 0, required = True, label = "Cantidad")
-    categoria = forms.ModelChoiceField(queryset = Categoria.objects.all())
+class itemForm(forms.Form):
+    nombre = forms.CharField(max_length = 60, required = True, label = "Nombre", widget=forms.TextInput(attrs={'style': 'width:100%'}))
+    cantidad = forms.IntegerField(max_value = 2147483647, min_value = 0, required = True, label = "Cantidad", widget=forms.NumberInput(attrs={'style': 'width:100%'}))
+    # Categoria.objects.none() creo que porque no hay nada,  si no seria Categoria.objects.all()
+    categoria = forms.ModelChoiceField(widget=forms.Select(attrs={'style': 'width:100%; background-color:white'}), queryset=Categoria.objects.none(), label = "Categoría")
     opciones_prioridad = (("baja", "Baja"),("media", "Media"), ("alta", "Alta"))
-    prioridad = forms.ChoiceField(required = True, choices = opciones_prioridad, widget = forms.Select(), label = "Prioridad")
-    minimo = forms.IntegerField(max_value = 2147483647, min_value = 0, required = True, label = "Mínimo valor para alerta")
+    prioridad = forms.ChoiceField(required = True, choices = opciones_prioridad, widget = forms.Select(attrs={'style': 'width:100%; background-color:white'}), label = "Prioridad")
+    minimo = forms.IntegerField(max_value = 2147483647, min_value = 0, required = True, label = "Mínimo valor para alerta", widget=forms.NumberInput(attrs={'style': 'width:100%'}))
